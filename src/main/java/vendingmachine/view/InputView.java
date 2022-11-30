93005cf78821360d130da2a0f59bc193a7032789
khj1 <dhepd8849@gmail.com>
2022-11-30 14:57:16 +0900

    feat: 상품명을 입력 받는 기능 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     private static final String REQUEST_HOLDING_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final String REQUEST_PRODUCTS = "상품명과 가격, 수량을 입력해 주세요.";
     private static final String REQUEST_INSERTED_MONEY = "투입 금액을 입력해 주세요.";
+    private static final String REQUEST_PRODUCT_NAME = "구매할 상품명을 입력해 주세요.";
 
     public Money readHoldingMoney() {
         System.out.println(REQUEST_HOLDING_MONEY);
@@ -26,5 +27,11 @@ public class InputView {
         System.out.println(REQUEST_INSERTED_MONEY);
 
         return Money.from(Console.readLine());
+    }
+
+    public String readProductName() {
+        System.out.println(REQUEST_PRODUCT_NAME);
+
+        return Console.readLine();
     }
 }
