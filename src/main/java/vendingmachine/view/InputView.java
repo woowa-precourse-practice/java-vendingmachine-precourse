19c1b1a0afb497e45f81335d9ac419284b1fd352
khj1 <dhepd8849@gmail.com>
2022-11-30 14:54:36 +0900

    feat: 투입 금액을 입력 받는 기능 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
 
     private static final String REQUEST_HOLDING_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final String REQUEST_PRODUCTS = "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String REQUEST_INSERTED_MONEY = "투입 금액을 입력해 주세요.";
 
     public Money readHoldingMoney() {
         System.out.println(REQUEST_HOLDING_MONEY);
@@ -19,5 +20,11 @@ public class InputView {
         System.out.println(REQUEST_PRODUCTS);
 
         return Products.of(Console.readLine());
+    }
+
+    public Money readInsertedMoney() {
+        System.out.println(REQUEST_INSERTED_MONEY);
+
+        return Money.from(Console.readLine());
     }
 }
