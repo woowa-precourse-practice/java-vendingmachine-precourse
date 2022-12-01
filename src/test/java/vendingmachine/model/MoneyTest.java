7e72b3602cdd7e42b67472ac8bbaeb9df97d7e9b
khj1 <dhepd8849@gmail.com>
2022-12-01 11:50:31 +0900

    style: static 임포트 수정

--- src/test/java/vendingmachine/model/MoneyTest.java
+++ src/test/java/vendingmachine/model/MoneyTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class MoneyTest {
 
