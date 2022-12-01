b4d251699ba41b46feb5488e20af253514d2a616
khj1 <dhepd8849@gmail.com>
2022-12-01 09:40:16 +0900

    refactor: 임의로 뽑는 동전 금액을 클래스 변수로 변경

--- src/main/java/vendingmachine/RandomCoinAmountGenerator.java
+++ src/main/java/vendingmachine/RandomCoinAmountGenerator.java
@@ -2,10 +2,14 @@ package vendingmachine;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.List;
+
 public class RandomCoinAmountGenerator implements CoinAmountGenerator {
+
+    private static final List<Integer> AMOUNTS = Coin.amounts();
 
     @Override
     public int generate() {
-        return Randoms.pickNumberInList(Coin.amounts());
+        return Randoms.pickNumberInList(AMOUNTS);
     }
 }
