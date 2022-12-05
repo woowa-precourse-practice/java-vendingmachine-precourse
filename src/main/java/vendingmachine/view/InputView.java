8a7a8256538147bf08cc49f7cf43cefdda7cec07
khj1 <dhepd8849@gmail.com>
2022-12-05 19:47:45 +0900

    feat: 구매 할 상품명을 입력 받는 기능 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -2,6 +2,7 @@ package vendingmachine.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import vendingmachine.model.Money;
+import vendingmachine.model.Name;
 import vendingmachine.model.Products;
 
 public class InputView {
@@ -9,6 +10,7 @@ public class InputView {
     private static final String REQUEST_HOLDING_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final String REQUEST_BALANCE_MESSAGE = "투입 금액을 입력해 주세요.";
     private static final String REQUEST_PRODUCTS_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String REQUEST_NAME_MESSAGE = "구매할 상품명을 입력해 주세요.";
 
     public Money readMoney() {
         System.out.println(REQUEST_HOLDING_MONEY_MESSAGE);
@@ -26,5 +28,11 @@ public class InputView {
         System.out.println(REQUEST_PRODUCTS_MESSAGE);
 
         return Products.from(Console.readLine());
+    }
+
+    public Name readName() {
+        System.out.println(REQUEST_NAME_MESSAGE);
+
+        return Name.from(Console.readLine());
     }
 }
