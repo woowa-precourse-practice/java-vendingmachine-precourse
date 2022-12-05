776c0771c88ce8ae9213793238442810c4cd17ab
khj1 <dhepd8849@gmail.com>
2022-12-05 19:44:58 +0900

    feat: 투입 금액을 입력 받는 기능 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -6,9 +6,16 @@ import vendingmachine.model.Money;
 public class InputView {
 
     private static final String REQUEST_HOLDING_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private static final String REQUEST_BALANCE_MESSAGE = "투입 금액을 입력해 주세요.";
 
     public Money readMoney() {
         System.out.println(REQUEST_HOLDING_MONEY_MESSAGE);
+
+        return Money.from(Console.readLine());
+    }
+
+    public Money readBalance() {
+        System.out.println(REQUEST_BALANCE_MESSAGE);
 
         return Money.from(Console.readLine());
     }
